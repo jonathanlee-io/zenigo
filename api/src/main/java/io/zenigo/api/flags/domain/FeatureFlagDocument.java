b67ecea4249ad95e3010f853c09203c02763fdff
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-04-14 05:06:06 +0100

    Refactor domain documents to improve code conciseness.
    Condensed Lombok annotations using wildcard imports and added `@EqualsAndHashCode` with `callSuper = true` to both domain classes. This simplifies the code and ensures proper equality checks by extending the base class attributes.

--- api/src/main/java/io/zenigo/api/flags/domain/FeatureFlagDocument.java
+++ api/src/main/java/io/zenigo/api/flags/domain/FeatureFlagDocument.java
@@ -1,10 +1,7 @@
 package io.zenigo.api.flags.domain;
 
 import io.zenigo.api.domain.AbstractDocument;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Document(collection = "feature_flags")
 public class FeatureFlagDocument extends AbstractDocument {
 
--- api/src/main/java/io/zenigo/api/flags/domain/FeatureFlagEnvironmentDocument.java
+++ api/src/main/java/io/zenigo/api/flags/domain/FeatureFlagEnvironmentDocument.java
@@ -1,10 +1,7 @@
 package io.zenigo.api.flags.domain;
 
 import io.zenigo.api.domain.AbstractDocument;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Document(collection = "feature_flag_environments")
 public class FeatureFlagEnvironmentDocument extends AbstractDocument {
 
