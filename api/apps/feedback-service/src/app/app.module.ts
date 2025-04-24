fa87e4c860dc0a8e9545da3a0bb828e3907966a5
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-04-24 21:19:03 +0100

    Simplify AppModule by removing unused imports
    Removed unused modules and configurations from AppModule to streamline the code. This change improves maintainability and ensures only necessary dependencies are included.

--- api/apps/feedback-service/src/app/app.module.ts
+++ api/apps/feedback-service/src/app/app.module.ts
@@ -1,15 +1,11 @@
-import {AuthModule} from '@app/auth';
 import {ConfigifyModule} from '@jdevel/configify';
-import {CacheModule} from '@nestjs/cache-manager';
 import {Module} from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
 import {EventEmitterModule} from '@nestjs/event-emitter';
-import {ThrottlerModule} from '@nestjs/throttler';
 
 import {EmbedScriptsModule} from './embed-scripts/embed-scripts.module';
 import {IssuesModule} from './issues/issues.module';
 import {ProductsModule} from './products/products.module';
-import {ClientsModule} from '../../../identity-service/src/clients/clients.module';
 
 @Module({
   imports: [
@@ -20,24 +16,9 @@ import {ClientsModule} from '../../../identity-service/src/clients/clients.modul
     EventEmitterModule.forRoot({
       global: true,
     }),
-    ThrottlerModule.forRoot([
-      {
-        ttl: 5_000,
-        limit: 10,
-      },
-    ]),
-    CacheModule.register({
-      isGlobal: true,
-    }),
-    AuthModule,
-    IssuesModule,
-    ClientsModule,
-    // UsersModule,
-    // ProjectsModule,
-    ProductsModule,
-    // PaymentsModule,
     EmbedScriptsModule,
+    IssuesModule,
+    ProductsModule,
   ],
-  providers: [],
 })
 export class AppModule {}
