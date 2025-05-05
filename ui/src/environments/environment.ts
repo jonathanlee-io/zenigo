595a3277a4b12b74e9926a3d1c365686aaa8b831
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-05-05 19:00:31 +0100

    Update Supabase URL and public key in environment config
    Replaced the Supabase URL and associated public key in the environment configuration file. This ensures the application connects to the correct Supabase instance with updated credentials.

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,9 +3,9 @@ export const environment = {
   local: true,
   API_ENVIRONMENT: 'local',
   WIDGET_URL: 'http://localhost:8080/widget.js',
-  SUPABASE_URL: 'https://wcxkdkfabmybehtjusdu.supabase.co',
+  SUPABASE_URL: 'https://uezywdolnrfzetsumznw.supabase.co',
   SUPABASE_PUBLIC_KEY:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjeGtka2ZhYm15YmVodGp1c2R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4NDk1MTMsImV4cCI6MjA1NjQyNTUxM30.W0ZJwu3OSu0Sn5JtfMdzD2RtpglZgivbwyKGTkm1EFU',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlenl3ZG9sbnJmemV0c3Vtem53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0Njc5NDksImV4cCI6MjA2MjA0Mzk0OX0.M_dNkjv08sJhW9xYArTdha2qOKYhOjJG2mOumunbbD8',
   FLAGSMITH_CLIENT_SDK_KEY: 'nxv6odmEDVsefCLVbUqgKd',
   FLAGSMITH_API_URL: 'https://flags.zenigo.io/api/v1/',
   PAYMENTS_SERVICE_BASE_URL: 'v1/payments',
