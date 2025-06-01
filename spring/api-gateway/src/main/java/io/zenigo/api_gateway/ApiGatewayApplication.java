270137ea916f613fdda1774af33b9bd174923dc1
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-06-01 01:21:10 +0100

    Add Node.js and Java CI workflows; configure api-gateway security and property management
    Introduced updates to CI workflows by refining job names for consistency and adding a new Java `spring-ci` workflow for Spring Boot projects. Implemented security configurations, JWT authentication setup, and CORS handling in `api-gateway`. Added application properties to support Redis, RabbitMQ, and JWT configurations.

--- spring/api-gateway/src/main/java/io/zenigo/api_gateway/ApiGatewayApplication.java
+++ spring/api-gateway/src/main/java/io/zenigo/api_gateway/ApiGatewayApplication.java
@@ -1,9 +1,12 @@
 package io.zenigo.api_gateway;
 
+import io.zenigo.api_gateway.config.AuthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = AuthProperties.class)
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
---
+++ spring/api-gateway/src/main/java/io/zenigo/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package io.zenigo.api_gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final AuthProperties authProperties;
+
+  @Value("${zenigo.cors.url}")
+  private String frontEndUrl;
+
+  @Bean
+  public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+    return http
+        .csrf(CsrfSpec::disable)
+        .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer ->
+            httpSecurityOAuth2ResourceServerConfigurer.jwt(
+                Customizer.withDefaults())
+        )
+        .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+        .build();
+  }
+
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.addAllowedOrigin(frontEndUrl);
+    corsConfig.addAllowedMethod("*");
+    corsConfig.addAllowedHeader("Authorization");
+    corsConfig.addAllowedHeader("Content-Type");
+    corsConfig.setAllowCredentials(true);
+    corsConfig.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfig);
+
+    return new CorsWebFilter(source);
+  }
+
+  @Bean
+  public ReactiveJwtDecoder reactiveJwtDecoder() {
+    return NimbusReactiveJwtDecoder
+        .withSecretKey(this.authProperties.getSecret())
+        .macAlgorithm(MacAlgorithm.HS512)
+        .build();
+  }
+}
