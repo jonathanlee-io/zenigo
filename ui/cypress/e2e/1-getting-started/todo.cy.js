231a13403f99ce13a50d5cf0572e45738ea9d4a6
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-06-06 03:19:21 +0100

    Remove Cypress advanced examples test files to streamline test suite and reduce clutter.

--- ui/cypress/e2e/1-getting-started/todo.cy.js
+++
@@ -1,9 +0,0 @@
-describe('example to-do app', () => {
-  beforeEach(() => {
-    cy.visit(Cypress.env('FRONT_END_URL' ?? 'http://localhost:4200'));
-  });
-
-  it('should load the landing page', async () => {
-    cy.url().should('include', '/');
-  });
-})
