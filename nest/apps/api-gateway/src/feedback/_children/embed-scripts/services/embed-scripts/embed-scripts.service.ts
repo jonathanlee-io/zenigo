2f2e3aa65299ea5f3ecd03a983becfb100551e50
Jonathan Lee <jonathan.lee.devel@gmail.com>
2025-06-09 04:40:49 +0100

    Implement Redis caching in `EmbedScriptsService` for bootstrap widget scripts, leveraging `CACHE_MANAGER` for efficient retrieval and storage.

--- nest/apps/api-gateway/src/feedback/_children/embed-scripts/services/embed-scripts/embed-scripts.service.ts
+++ nest/apps/api-gateway/src/feedback/_children/embed-scripts/services/embed-scripts/embed-scripts.service.ts
@@ -5,8 +5,10 @@ import {
 } from '@app/comms';
 import {FeedbackServiceContract} from '@app/comms/contracts/feedback-service';
 import {HttpHelpersUtil} from '@app/util';
+import {CACHE_MANAGER} from '@nestjs/cache-manager';
 import {Inject, Injectable, Logger} from '@nestjs/common';
 import {ClientProxy} from '@nestjs/microservices';
+import {Cache} from 'cache-manager';
 
 @Injectable()
 export class EmbedScriptsService {
@@ -19,6 +21,7 @@ export class EmbedScriptsService {
     private readonly logger: Logger,
     @Inject(FEEDBACK_SERVICE_QUEUE)
     readonly untypedFeedbackClient: ClientProxy,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {
     this.feedbackClient = new TypedClientProxy(untypedFeedbackClient);
   }
@@ -30,6 +33,12 @@ export class EmbedScriptsService {
     clientSubdomain: string;
     ip: string;
   }) {
+    const fromCacheResult = await this.cacheManager.get(
+      `bootstrap-widget-script-${clientSubdomain.toLowerCase()}`,
+    );
+    if (fromCacheResult) {
+      return fromCacheResult;
+    }
     const result = await this.feedbackClient.sendAsync(
       FEEDBACK_SERVICE.GET_BOOTSTRAP_SCRIPT,
       {
@@ -37,6 +46,10 @@ export class EmbedScriptsService {
         clientIp: ip,
         data: null as never,
       },
+    );
+    await this.cacheManager.set(
+      `bootstrap-widget-script-${clientSubdomain.toLowerCase()}`,
+      result.data,
     );
     return HttpHelpersUtil.returnDataOrThrowError(result);
   }
